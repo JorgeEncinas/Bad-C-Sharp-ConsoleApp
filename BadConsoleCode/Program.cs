7f15dd80711131e629fd00f3b0c250dd34c66f38
alegre <jorgecarlosencinas@gmail.com>
2021-10-12 20:26:53 -0700

    First commit

---
+++ BadConsoleCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace BadConsoleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; //Variables that won't ever be used
+            int y = 2;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fool, you have fallen into my infinite loop trap!");
+            Console.WriteLine("We'll be here forever and ever...");
+            for (int i = 0; i < i+1; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("And ever...");
+            }
+        }
+    }
+}
